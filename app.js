1ed52270432226c7e39dd6484f0c2357fac68692
Ramkrishna Sharma <ram@ramkrishnas-mbp.myfiosgateway.com>
2023-01-10 18:58:41 -0500

    final commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const getDadJoke = async () => {
     const config = { headers: { Accept: 'application/json' } }
     const res = await axios.get('https://icanhazdadjoke.com/', config);
@@ -6,6 +7,7 @@ const getDadJoke = async () => {
 
 const jokes = document.querySelector('#jokes');
 const button = document.querySelector('#btn');
+
 
 const addNewJoke = async () => {
     const jokeText = await getDadJoke();
